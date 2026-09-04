bd1d7391f3440515cb825b6374e47d8bf1cfdf00
faisaljj0230-commits <faisaljj0230@gmail.com>
2026-09-04 05:18:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,9 @@
+// Central place for anything that might need to change later.
+// The Discord link is used everywhere in the site through this file only.
 
+const DISCORD_LINK = 'https://discord.gg/zwdhGyX2P';
+
+const SITE = {
+  brand: 'VEX CODE',
+  year: 2026
+};
